e75d1da92812cc335fa1618406d7ea23e861f1bc
山田寛治 <yamada@cockroachg.work>
2022-03-15 21:10:11 +0900

    [skip ci] memo

---
+++ packages/web/src/components/CastChannel.vue
@@ -0,0 +1,12 @@
+<template v-for="channel of group.channels">
+  <label>{{ channel.label }}</label>
+  <cast-switch v-model="channel.cast"></cast-switch>
+  <preview-switch v-model="channel.preview"></preview-switch>
+  <microphone-icon v-if="channel.kind === 'audioinput'"></microphone-icon>
+  <speaker-icon v-if="channel.kind === 'audiooutput'"></speaker-icon>
+  <camera-icon v-if="channel.kind === 'videoinput'"></camera-icon>
+  <template v-if="channel.preview">
+    <audio v-if="channel.kind.startsWith('audio')"></audio>
+    <video v-if="channel.kind.startsWith('video')"></video>
+  </template>
+</template>
--- packages/web/src/views/Cast.vue
+++ packages/web/src/views/Cast.vue
@@ -13,39 +13,13 @@
           },
         ]"
       >
-        <CastChannelList />
-        <template v-for="channel of group.channels">
-          <label>{{ channel.label }}</label>
-          <cast-switch v-model="channel.cast"></cast-switch>
-          <preview-switch v-model="channel.preview"></preview-switch>
-          <microphone-icon
-            v-if="channel.kind === 'audioinput'"
-          ></microphone-icon>
-          <speaker-icon v-if="channel.kind === 'audiooutput'"></speaker-icon>
-          <camera-icon v-if="channel.kind === 'videoinput'"></camera-icon>
-          <template v-if="channel.preview">
-            <audio v-if="channel.kind.startsWith('audio')"></audio>
-            <video v-if="channel.kind.startsWith('video')"></video>
-          </template>
-        </template>
+      <template v-for="channel of group.channels">
+        <CastChannel v-model="channel.cast" />
       </template>
     </template>
     <template v-if="!grouping">
-      <template
-        v-for="channel of [
-          { label: '1', kind: 'audioinput', cast: false, preview: true },
-        ]"
-      >
-        <label>{{ channel.label }}</label>
-        <cast-switch v-model="channel.cast"></cast-switch>
-        <preview-switch v-model="channel.preview"></preview-switch>
-        <microphone-icon v-if="channel.kind === 'audioinput'"></microphone-icon>
-        <speaker-icon v-if="channel.kind === 'audiooutput'"></speaker-icon>
-        <camera-icon v-if="channel.kind === 'videoinput'"></camera-icon>
-        <template v-if="channel.preview">
-          <audio v-if="channel.kind.startsWith('audio')"></audio>
-          <video v-if="channel.kind.startsWith('video')"></video>
-        </template>
+      <template v-for="channel of []">
+        <CastChannel v-model="channel.cast" />
       </template>
     </template>
     <button>add screen</button>
